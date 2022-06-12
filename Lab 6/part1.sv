525445c09c70bf03ae3a334df71c5e0fd712c5af
ishmemon <107277338+ishmemon@users.noreply.github.com>
2022-06-11 18:19:51 -0700

    Update part1.sv

--- Lab 6/part1.sv
+++ Lab 6/part1.sv
@@ -1,3 +1,4 @@
+//Attribution: This file was contributed by the UW Electrical Engineering Faculty.
 module part1 (CLOCK_50, CLOCK2_50, KEY, FPGA_I2C_SCLK, FPGA_I2C_SDAT, AUD_XCK, 
 		        AUD_DACLRCK, AUD_ADCLRCK, AUD_BCLK, AUD_ADCDAT, AUD_DACDAT);
 
